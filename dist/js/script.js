0bbddc3b90e1fd6b4b2c31eb25330e8057bae2d5
Mohamad Feby <mhmdfeby6@gmail.com>
2024-08-25 00:54:25 +0700

    portofolio tailwind css

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,13 +9,28 @@ toggle1.addEventListener('click', function(){
 });
 
 //navbar
-window.onscroll = function() {
-    const header = document.querySelector('header')
-    const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
+
+//darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+
+});
+
+//togglemove
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 }
+
